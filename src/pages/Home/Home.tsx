92b7b2a38810ea24ff0cac9654cc725bc9a697f0
Paulo <pmarquesbaronefilho18@protonmail.com>
2024-02-28 16:58:10 -0300

    add: background images on product section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import Header from '../../components/Header'
 import homeImg from '../../assets/home-img.png'
 import butterflyIcon from '../../assets/background-icons/butterfly.svg'
 import bulbIcon from '../../assets/background-icons/bulb.svg'
+import plantIcon from '../../assets/background-icons/plant-2.svg'
 import ChooseOption from '../../components/ChooseOption'
 import { chooseOptions } from '../../data/chooseOptions'
 import { products } from '../../data/products'
@@ -24,9 +25,9 @@ export default function Home() {
           </div>
           <img className='translate-x-1/4 sm:translate-x-1/2 sm:max-w-lg' src={homeImg} alt='Sacola sustentável' />
 
-          <img className='absolute top-44 right-[10%] w-[10%] max-w-24' src={butterflyIcon} />
-          <img className='absolute top-60 left-[20%] w-[8%] max-w-12 opacity-60' src={butterflyIcon} />
-          <img className='absolute bottom-16 left-[10%] w-[15%] max-w-36' src={bulbIcon} />
+          <img className='absolute top-44 right-[10%] w-[10%] max-w-24 -z-10' src={butterflyIcon} />
+          <img className='absolute top-60 left-[20%] w-[8%] max-w-12 opacity-60 -z-10' src={butterflyIcon} />
+          <img className='absolute bottom-16 left-[10%] w-[15%] max-w-36 -z-10' src={bulbIcon} />
 
           <div className='w-2/5 max-w-60 h-1 bg-zinc-500 rounded-lg'></div>
         </div>
@@ -41,7 +42,8 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='py-6 px-2 flex flex-col justify-center items-center'>
+      <section className='py-6 px-2 flex flex-col justify-center items-center relative'>
+        <img className='absolute -top-10 right-[10%] translate-x-1/2 -z-10 min-w-56 w-[25%] max-w-[300px]' src={plantIcon} />
         <div className='responsive-container flex flex-col items-center gap-8'>
           <h2 className='text-gray text-2xl font-bold text-center'>Nossos Produtos Sustentáveis Mais Vendidos</h2>
           <div className='grid grid-cols-1 gap-12'>
@@ -50,6 +52,7 @@ export default function Home() {
             ))}
           </div>
         </div>
+        <img className='absolute -bottom-28 left-[10%] -translate-x-1/2 -z-10 min-w-56 w-[25%] max-w-[300px] rotate-45' src={plantIcon} />
       </section>
     </>
   )
